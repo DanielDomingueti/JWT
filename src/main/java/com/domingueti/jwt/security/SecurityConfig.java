0bb49bd7e2dd98dc3f69371d1385a80c17300c7d
DanielDomingueti <danieldomingueti@hotmail.com>
2021-11-15 09:12:43 -0300

    authentication methods

--- src/main/java/com/domingueti/jwt/security/SecurityConfig.java
+++ src/main/java/com/domingueti/jwt/security/SecurityConfig.java
@@ -1,12 +1,17 @@
 package com.domingueti.jwt.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.domingueti.jwt.filter.CustomAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -25,9 +30,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-//		http.csrf().
+		http.csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeRequests().anyRequest().permitAll();
+		http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+		
 	}
 
-	
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
 	
 }
